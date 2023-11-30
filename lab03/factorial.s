1206d2fd7b4f6bc4a193da0f48fa2bbda07387a7
tufailR <tufail.rivi512@gmail.com>
2023-11-30 17:48:32 +0500

    lab03 Completed

--- lab03/factorial.s
+++ lab03/factorial.s
@@ -5,8 +5,8 @@ n: .word 8
 
 .text
 main:
-    la t0, n
-    lw a0, 0(t0)
+    la a0, n      # Load the address of n into a0
+    lw a0, 0(a0)  # Load the value of n into a0
     jal ra, factorial
 
     addi a1, a0, 0
@@ -22,3 +22,17 @@ main:
 
 factorial:
     # YOUR CODE HERE
+   
+  addi t0, x0, 1       
+  addi t1, x0, 1       
+  
+loop:
+  bgt t0, a0, exit    
+  mul t1, t1, t0       
+  addi t0, t0, 1       
+  jal x0, loop
+  
+exit:
+  add a0, t1, x0       
+  ret
+
